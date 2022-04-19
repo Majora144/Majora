b4ab17bdf7cbef96522a2d12ab8f7d1fa57ea8e1
Majora <97760119+Majora144@users.noreply.github.com>
2022-04-19 19:10:41 +0200

    Create Example.lua

---
+++ Notification System Iris/Example.lua
@@ -0,0 +1,16 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Majora144/Majora/main/Notification%20System%20Iris/source.lua"))()
+
+Notification.Notify("Yeeet", "yeet", "rbxasset://textures/ui/GuiImagePlaceholder.png", {
+    Duration = 2,       
+    Main = {
+        Rounding = true,
+    }
+});
+
+Notification.WallNotification("Test", "The Fitness Gram Pacer Test", {
+        Duration = 3,
+
+        TitleSettings = {
+            Enabled = false
+        }
+    });
