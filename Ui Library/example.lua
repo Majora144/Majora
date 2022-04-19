4e3e3692c785dcd8cdba9c255c0537cc502ba18b
Majora <97760119+Majora144@users.noreply.github.com>
2022-04-19 19:57:31 +0200

    Create example.lua

---
+++ Ui Library/example.lua
@@ -0,0 +1,12 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Majora144/Ui-libs-back-ups/main/Uwuware%20UI", true))()
+
+local Window = Library:CreateWindow("thing")
+
+local folder = Window:AddFolder('stuff')
+folder:AddToggle({text = "This is a toggle", flag = "toggle", callback = function(v) print(v) end})
+folder:AddButton({text = 'button', flag = "button", callback = function() print('button clicker') end})
+folder:AddSlider({text = 'slider', min = 1, max = 100, flag = "slider", callback = function(v) print(v) end})
+folder:AddList({text = 'option', values = {'yo', 'yes'}, flag = "list", callback = function(v) print(v) end})
+folder:AddBox({text = 'text box', flag = "box", callback = function(v) print(v) end})
+folder:AddColor({text = 'color', flag = "color", callback = function(v) print(v) end})
+Library:Init()
